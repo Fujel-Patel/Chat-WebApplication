3b1dcae9f94dd4f47a3f36cf12ad1c6d51aceda9
@fujel <fujelpatel6013@gmail.com>
2025-05-19 17:57:43 +0530

    update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
 import path from "path"
+dotenv.config();
 
 import { connectDB } from "./lib/db.js"; 
 
