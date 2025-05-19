e2cf918604b6c061d5710568a82c3e89e2db952a
@fujel <fujelpatel6013@gmail.com>
2025-05-19 21:33:29 +0530

    finally

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
 import path from "path"
-dotenv.config();
 
-import { connectDB } from "./lib/db.js"; 
+import { connectDB } from "./src/lib/db.js"; 
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
