5a2928d7a3758e6c15cff2e386cb01e86dc7200b
@fujel <fujelpatel6013@gmail.com>
2025-05-20 09:38:16 +0530

    paytu

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser()); 
 app.use(cors({
-  origin: "chat-web-application-eight-orpin.vercel.app", // your frontend port
+  origin: "http://localhost:5173/", // your frontend port
   credentials: true,
 }));
 
