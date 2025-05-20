57ff69154a27bb81a52d7d24bcba3b2253557cc0
@fujel <fujelpatel6013@gmail.com>
2025-05-20 10:11:53 +0530

    thai gayu la

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors"
@@ -12,9 +12,8 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-// const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
