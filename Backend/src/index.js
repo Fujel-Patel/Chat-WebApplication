9a12dcfd96c11c7785b00aa0f6d8741c9f34c63c
@fujel <fujelpatel6013@gmail.com>
2025-05-20 12:00:32 +0530

    have thai gyu bhai

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -18,73 +18,30 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173/", // your frontend port
-  credentials: true,
+  origin: "http://localhost:5173/", // your frontend port
+  credentials: true,
 }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
-  });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
 }
 
 connectDB()
-  .then(() => {
-    console.log("Database Connected Successfully");
-    // ... server startup ...
-  })
-  .catch((error) => {
-    console.error("database not connected:", error); // Log the error here
-  });
-
-// import express from "express";
-// import 'dotenv/config';
-// import cookieParser from "cookie-parser";
-// import cors from "cors"
-
-// import path from "path"
-
-// import { connectDB } from "./lib/db.js"; 
-
-// import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
-// import { app, server } from "./lib/socket.js";
-
-// const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
-
-// app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-
-// app.use(cookieParser()); 
-// app.use(cors({
-//   origin: "http://localhost:5173/", // your frontend port
-//   credentials: true,
-// }));
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/message", messageRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-
-// connectDB()
-//   .then(() => {
-//     console.log("Database Connected Successfully");
-//     server.listen(PORT, () => {
-//       console.log(`app run on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("database not connected");
-//   });
+  .then(() => {
+    console.log("Database Connected Successfully");
+    const server = http.createServer(app);
+    io.attach(server);
+    server.listen(PORT, () => {
+      console.log(`app run on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("database not connected:", error); // Log the error here
+  });
