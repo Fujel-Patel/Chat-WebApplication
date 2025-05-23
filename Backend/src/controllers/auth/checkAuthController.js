cc7920d77f05d70339425344711bfff7ed514287
@fujel <fujelpatel6013@gmail.com>
2025-05-23 21:13:27 +0530

    06

--- Backend/src/controllers/auth/checkAuthController.js
+++ Backend/src/controllers/auth/checkAuthController.js
@@ -1,8 +1,3 @@
-import { generateToken } from "../lib/utils.js";
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
-import cloudinary from "../lib/cloudinary.js";
-
 export const checkAuth = (req, res) => {
   try {
     res.status(200).json(req.user);
