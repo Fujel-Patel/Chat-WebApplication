017501f03e276854648b3ec6f3cb0536b3b636c1
@fujel <fujelpatel6013@gmail.com>
2025-05-25 00:41:14 +0530

    2

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,10 @@
+import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
-import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput";
-import MessageSkeleton from "./skeletons/MessageSkeleton";
-import { useAuthStore } from "../store/useAuthStore";
+import ChatHeader from "./ChatHeader.jsx"
+import MessageInput from "./MessageInput.jsx";
+import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
