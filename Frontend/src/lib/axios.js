8f3601b73fcf0096c2b52fe5a74595a8e99d4aa0
@fujel <fujelpatel6013@gmail.com>
2025-05-24 22:50:45 +0530

    07

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5000/api"
-  : "https://chat-webapplication-yf2z.onrender.com";
+  : "https://chat-webapplication-yf2z.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
